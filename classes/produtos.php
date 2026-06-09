3349e892e42a7342930000bc65780968cb9bb0fc
caua-mourawx <cauajoseferreiramoura@gmail.com>
2026-06-09 20:24:36 -0300

    Update produtos.php

--- classes/produtos.php
+++ classes/produtos.php
@@ -1,46 +1,15 @@
 <?php
+declare(strict_types=1);
 
-class Produto {
+class Produto implements ProdutoInterface
+{
+    public function __construct(
+        private string $nome,
+        private float $preco
+    ) {}
 
-    private $nome;
-    private $preco;
-    private $quantidade;
-
-    public function __construct($nome, $preco, $quantidade) {
-        $this->nome = $nome;
-        $this->preco = $preco;
-        $this->quantidade = $quantidade;
-    }
-
-    public function getNome() {
-        return $this->nome;
-    }
-
-    public function setNome($nome) {
-        $this->nome = $nome;
-    }
-
-    public function getPreco() {
-        return $this->preco;
-    }
-
-    public function setPreco($preco) {
-        $this->preco = $preco;
-    }
-
-    public function getQuantidade() {
-        return $this->quantidade;
-    }
-
-    public function setQuantidade($quantidade) {
-        $this->quantidade = $quantidade;
-    }
-
-    public function exibirProduto() {
-        echo "Produto: " . $this->nome . "<br>";
-        echo "Preço: R$ " . number_format($this->preco, 2, ',', '.') . "<br>";
-        echo "Quantidade em estoque: " . $this->quantidade . "<br><br>";
+    public function exibir(): string
+    {
+        return "{$this->nome} - R$ {$this->preco}";
     }
 }
-
-?>
